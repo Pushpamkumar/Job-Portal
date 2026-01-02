e00f27765689208c44c1a84ddaafb4fbd1c640b3
pushpam <pushpamkumar669@gmail.com>
2026-01-02 15:08:12 +0530

    job

---
+++ backend/routes/company.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import { getCompany, getCompanyById, registerCompany, updateCompany } from "../controllers/company.controller.js";
+import { singleUpload } from "../middlewares/mutler.js";
+
+const router = express.Router();
+
+// 👇 REMOVE `isAuthenticated` here
+router.route("/register").post(registerCompany);
+router.route("/get").get(isAuthenticated,getCompany);
+router.route("/get/:id").get(isAuthenticated,getCompanyById);
+router.route("/update/:id").put(isAuthenticated,singleUpload, updateCompany);
+
+export default router;
+
